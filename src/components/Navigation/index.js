259129e357093a51f34ec28834b8e9d3334c5b68
ZGS <zgstowell@gmail.com>
2020-08-30 13:12:27 -0700

    updated bg image

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -19,56 +19,52 @@ export default function Index(props) {
         // <Nav className="col-md-12 d-none d-md-block bg-light sidebar">
         // <Navbar variant="dark" expand="md">
         <div className="nav-custom">
-        <Navbar collapseOnSelect expanded={expanded} expand="lg" variant='dark'>
-       
-            <Navbar.Toggle
-            className="nav-button"
-            aria-controls="responsive-navbar-nav"
-            onClick={() => setExpanded(expanded ? false : 'expanded')} />
-            {/* <Navbar.Toggle as={{<Button>}} /> */}
-            
-            <Navbar.Collapse id="responsive-navbar-nav" className="testing">
-            
-       
+            <Navbar collapseOnSelect expanded={expanded} expand="lg" variant='dark'>
 
-            <Nav className="flex-column sidebar">
-            <Navbar.Brand href="/home">
-            <img
-                src={faceIcon}
-                // width="30"
-                height="75"
-                className="d-inline-block align-top"
-                alt="React Bootstrap logo"
+                <Navbar.Toggle
+                    className="nav-button"
+                    aria-controls="responsive-navbar-nav"
+                    onClick={() => setExpanded(expanded ? false : 'expanded')} />
+                <Navbar.Collapse id="responsive-navbar-nav" className="testing">
+                    <Nav className="flex-column sidebar">
+                        <Navbar.Brand href="/home">
+                            <img
+                                src={faceIcon}
+                                // width="30"
+                                height="75"
+                                className="d-inline-block align-top"
+                                alt="React Bootstrap logo"
 
-            />
-            </Navbar.Brand> 
-                {props.routes.map(route => (
-                    <Nav.Link
-                    onClick={() => setExpanded(false)}
-                    key={route.path}
-                    as={NavLink}
-                    to={route.path}
-                    activeClassName="active"
-                    exact
-                    >
-                        {route.name}
-                    </Nav.Link>
-                ))}
-                <Nav.Link
-                // as={NavLink}
-                onClick={() => setModalShow(true)}
-                to="#"
-                >
-                Resum&eacute;
+                            />
+                        </Navbar.Brand>
+                        {props.routes.map(route => (
+                            <Nav.Link
+                                onClick={() => setExpanded(false)}
+                                key={route.path}
+                                as={NavLink}
+                                to={route.path}
+                                activeClassName="active"
+                                exact
+                            >
+                                {route.name}
+                            </Nav.Link>
+                        ))}
+                        <Nav.Link
+                            // as={NavLink}
+                            onClick={() => setModalShow(true)}
+                            // onClick={() => setExpanded(false)}
+                            to="#"
+                        >
+                            Resum&eacute;
                 </Nav.Link>
-                <ReadmeModal
-                    show={modalShow}
-                    onHide={() => setModalShow(false)}
-                    
-                />
-            </Nav>
+                        <ReadmeModal
+                            show={modalShow}
+                            onHide={() => setModalShow(false)}
+
+                        />
+                    </Nav>
                 </Navbar.Collapse>
-        </Navbar>
+            </Navbar>
         </div>
     )
 }
