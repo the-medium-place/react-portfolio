6c2e2e61e4186a75731d4b1b6a1597317bd460eb
Zac <zgstowell@gmail.com>
2020-11-11 19:53:51 -0800

    first commit in new branch, dropped all components

--- src/pages/ContactMe/index.js
+++ src/pages/ContactMe/index.js
@@ -1,88 +1,9 @@
-import React from 'react';
-import './style.css';
-import API from '../../utils/API';
-// import ContactForm from '../../components/ContactForm';
-import { Row, Col } from 'react-bootstrap';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(fab, fas);
+import React from 'react'
 
-
-
-export default class ContactMe extends React.Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            contactName: '',
-            contactEmail: '',
-            contactMessage: ''
-        }
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        const messageObj = {
-            username: this.state.contactName,
-            userEmail: this.state.contactEmail,
-            userMessage: this.state.contactMessage
-        }
-        API.sendMail(messageObj)
-
-        // setTimeout(function () {
-        //     window.location.reload();
-        // }, 0);
-    };
-
-    handleInputChange = event => {
-        const value = event.target.value;
-        const name = event.target.name;
-        this.setState({
-            [name]: value
-        });
-        console.log(this.state)
-    };
-
-    render() {
-        return (
-            <>
-                <div className="contact-card">
-                    <Row>
-                        <Col>
-                            <h4>Reach Out!</h4>
-                        </Col>
-                    </Row>
-                    <Row className="small-cards">
-                        <Col className="contact-card small-card">
-                            <a target="_blank" rel="noopener noreferrer" href="mailto:zgstowell@gmail.com">
-                                <h1><FontAwesomeIcon icon={["fas", "envelope"]} /></h1>
-                                <small>zgstowell@gmail.com</small>
-                            </a>
-                        </Col>
-                        <Col className="contact-card small-card">
-                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/the-medium-place">
-                                <h1><FontAwesomeIcon icon={["fab", "github"]} /></h1>
-                                <small>github.com/the-medium-place</small>
-                            </a>
-                        </Col>
-                        <Col className="contact-card small-card">
-                            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/zachary-stowell/">
-                                <h1><FontAwesomeIcon icon={["fab", "linkedin"]} /></h1>
-                                <small>linkedin.com/in/zachary-stowell</small>
-                            </a>
-                        </Col>
-                    </Row>
-                </div>
-
-                <div className="contact-form-box">
-                    Contact form coming soon!
-                    {/* <ContactForm handleInputChange={this.handleInputChange} handleSubmit={this.handleSubmit} /> */}
-                </div>
-            </>
-        )
-    }
+export default function ContactMe() {
+    return (
+        <div>
+            
+        </div>
+    )
 }
-
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,66 +1,9 @@
-import React, { Component } from 'react';
-import API from '../../utils/API';
-import PortfolioCard from '../../components/PortfolioCard';
-import './style.css';
-import LoadingIcon from '../../components/LoadingIcon';
+import React from 'react'
 
-export default class Portfolio extends Component {
-
-    state = {
-        reposArr: []
-    }
-
-    // GET MY REPOS FROM GITHUB API
-    componentDidMount() {
-        API.getRepos()
-            .then(res => {
-                // FILTER RESULT TO REMOVE 
-                // REPOS WITHOUT 'homepage' VALUE
-                res.data = res.data.filter(repo => repo.homepage !== null)
-                .filter(repo => repo.homepage !== '');
-              
-                // console.log(res.data)
-                // ADD LANGUAGES TO STATE OBJ
-                res.data.forEach((repo) => {
-                    API.getRepoLanguages(repo.name)
-                    .then(langs => {
-                        // console.log(langs.data)
-                        repo.languageObj = langs.data;
-                        this.setState({ reposArr: res.data });
-                    })
-
-                })
-            })
-            .catch(err => console.error(err))
-    }
-
-
-    render() {
-        return (
-            <div className="portfolio-wrapper">
-                {/* CHECK FOR API RESULTS AND LOAD THEM IF PRESENT */}
-                {this.state.reposArr.length > 0 ? this.state.reposArr.sort((a, b) => b.size - a.size).map((repo, index) => {
-                //    console.log(repo)
-                    return (
-
-                        <PortfolioCard 
-                        key={index} 
-                        readmeURL={repo.readmeURL} 
-                        html_url={repo.html_url} 
-                        name={repo.name} 
-                        description={repo.description} 
-                        updated_at={repo.updated_at} 
-                        homepage={repo.homepage} 
-                        languageObj={(repo.languageObj)} />
-                    )
-                }) : (
-                        <div className="loading-wrapper">
-                            <LoadingIcon />
-                            <h2>Loading Portfolio...</h2>
-                        </div>
-                    )}
-            </div>
-        )
-    }
+export default function Portfolio() {
+    return (
+        <div>
+            
+        </div>
+    )
 }
-
